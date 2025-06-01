2cc03351640b7ffa85ec8367ab71b79281a10481
Ganda Dwiyana <dwiyanaganda3@gmail.com>
2025-06-01 17:24:19 +0700

    Resolve merge conflict in api.php and web.php

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\API\JadwalApiController;
 use App\Http\Controllers\JadwalDokterController;
-use App\Http\Controllers\API\PemeriksaanApiController;
+use App\Http\Controllers\API\BookingAPIController;
 
+// 🔹 Jadwal Dokter
 Route::get('/jadwal', [JadwalDokterController::class, 'index']);
-Route::get('/jadwals/{id}', [JadwalDokterController::class, 'show']);
 Route::get('/jadwals', [JadwalApiController::class, 'index']);
+Route::get('/jadwals/{id}', [JadwalDokterController::class, 'show']);
 
+// 🔹 Booking Kunjungan
+Route::get('/booking', [BookingAPIController::class, 'index']);
+Route::get('/booking/{id}', [BookingAPIController::class, 'show']);
+
+// 🔹 Pemeriksaan (HARUS LOGIN)
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/riwayat-kunjungan', [PemeriksaanApiController::class, 'index']); // Mahasiswa
     Route::get('/riwayat-kunjungan/{id}', [PemeriksaanApiController::class, 'show']); // Mahasiswa
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JadwalDokterController;
 
-Route::resource('jadwals', JadwalDokterController::class); 
+use App\Http\Controllers\JadwalDokterController;
+use App\Http\Controllers\BookingController;
+
+// 🔹 Home
 Route::get('/', [JadwalDokterController::class, 'index']);
+
+// 🔹 Jadwal Dokter (Admin)
+Route::resource('jadwals', JadwalDokterController::class); 
 Route::get('/jadwals', [JadwalDokterController::class, 'index'])->name('jadwals.index');
 Route::get('/jadwals/create', [JadwalDokterController::class, 'create'])->name('jadwals.create');
 Route::post('/jadwals', [JadwalDokterController::class, 'store'])->name('jadwals.store');
 
-
+// 🔹 Booking Kunjungan (Mahasiswa)
+Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');          
+Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create');  
+Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');          
+Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit'])->name('bookings.edit'); 
+Route::put('/bookings/{booking}', [BookingController::class, 'update'])->name('bookings.update');  
+Route::delete('/bookings/{booking}', [BookingController::class, 'destroy'])->name('bookings.destroy'); 
